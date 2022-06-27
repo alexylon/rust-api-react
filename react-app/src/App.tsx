6c7c958a991467bab50a568dceffd5f499238238
Alexander Alexandrov <aalexandrov4@dxc.com>
2022-06-27 21:41:39 +0300

    Change rest server port

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Card, Box, Grid} from '@mui/material';
 
 const App = () => {
     const [persons, setPersons] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:8000/customers')
+        fetch('http://localhost:3030/customers')
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
@@ -14,40 +14,43 @@ const App = () => {
                 console.log(err.message);
             });
     }, []);
-    console.log("persons: ", persons);
 
     return (
-        <>
-            <TableContainer component={Paper}>
-                <Table sx={{minWidth: 650}} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>guid</TableCell>
-                            <TableCell align="right">First Name</TableCell>
-                            <TableCell align="right">Last Name</TableCell>
-                            <TableCell align="right">Email</TableCell>
-                            <TableCell align="right">Address</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {persons.map((person, i) => (
-                            <TableRow
-                                key={i}
-                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {person["guid"]}
-                                </TableCell>
-                                <TableCell align="right">{person["first_name"]}</TableCell>
-                                <TableCell align="right">{person["last_name"]}</TableCell>
-                                <TableCell align="right">{person["email"]}</TableCell>
-                                <TableCell align="right">{person["address"]}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
+        <Grid container spacing={2}>
+            <Box marginTop={5} marginLeft={20} marginRight={20}>
+                <Card raised>
+                    <TableContainer component={Paper}>
+                        <Table sx={{minWidth: 650}} aria-label="simple table">
+                            <TableHead>
+                                <TableRow style={{fontWeight: "bold"}}>
+                                    <TableCell>guid</TableCell>
+                                    <TableCell align="right">First Name</TableCell>
+                                    <TableCell align="right">Last Name</TableCell>
+                                    <TableCell align="right">Email</TableCell>
+                                    <TableCell align="right">Address</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {persons.map((person, i) => (
+                                    <TableRow
+                                        key={i}
+                                        sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {person["guid"]}
+                                        </TableCell>
+                                        <TableCell align="right">{person["first_name"]}</TableCell>
+                                        <TableCell align="right">{person["last_name"]}</TableCell>
+                                        <TableCell align="right">{person["email"]}</TableCell>
+                                        <TableCell align="right">{person["address"]}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Card>
+            </Box>
+        </Grid>
     );
 };
 
--- rust-warp-api/src/main.rs
+++ rust-warp-api/src/main.rs
@@ -10,7 +10,8 @@ async fn main() {
     let db = db::init_db();
     let customer_routes = routes::customer_routes(db);
 
+    println!("Warp Server is running!");
     warp::serve(customer_routes)
-        .run(([127, 0, 0, 1], 8000))
+        .run(([127, 0, 0, 1], 3030))
         .await;
 }
